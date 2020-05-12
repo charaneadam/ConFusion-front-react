7f9db0b0a2a7721190a8c7f8ea61a5ea2a5de99e
Adam Charane <adam.charane@nimbleways.com>
2020-05-12 16:17:59 +0000

    Refactoring: dividing contact component to smaller functional components

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -15,55 +15,79 @@ class Contact extends Component {
       contactType: 'Tel.',
       message: ''
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+
+  }
+
+  handleSubmit(event) {
+    console.log("current state is : " + JSON.stringify(this.state));
+    alert("current state is : " + JSON.stringify(this.state));
+    event.preventDefault();
   }
 
   render() {
-    return (
-      <div className="container">
 
+    const BreadCrumb = () => {
+      return (
         <div className="row">
           <Breadcrumb>
             <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
             <BreadcrumbItem active>Contact us</BreadcrumbItem>
           </Breadcrumb>
-          <div className="col-12">
-            <h3>Contact us</h3><hr />
-          </div>
         </div>
+      )
+    }
 
-        <div className="row row-content">
-          <div className="col-12">
-            <h3>Location Information</h3>
-          </div>
-          <div className="col-12 col-sm-4 offset-sm-1">
-            <h5>Our Address</h5>
-            <address>
-              121, Clear Water Bay Road<br />
-                            Clear Water Bay, Kowloon<br />
-                            HONG KONG<br />
-              <i className="fa fa-phone"></i>: +852 1234 5678<br />
-              <i className="fa fa-fax"></i>: +852 8765 4321<br />
-              <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
-            </address>
-          </div>
-          <div className="col-12 col-sm-6 offset-sm-1">
-            <h5>Map of our Location</h5>
-          </div>
-          <div className="col-12 col-sm-11 offset-sm-1">
-            <div className="btn-group" role="group">
-              <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-              <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-              <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+    const ContactUsInfos = () => {
+      return (
+        <>
+          <div className="row">
+            <div className="col-12">
+              <h3>Contact us</h3><hr />
             </div>
           </div>
-        </div>
 
+          <div className="row row-content">
+            <div className="col-12">
+              <h3>Location Information</h3>
+            </div>
+            <div className="col-12 col-sm-4 offset-sm-1">
+              <h5>Our Address</h5>
+              <address>
+                121, Clear Water Bay Road<br />
+                            Clear Water Bay, Kowloon<br />
+                            HONG KONG<br />
+                <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+              </address>
+            </div>
+            <div className="col-12 col-sm-6 offset-sm-1">
+              <h5>Map of our Location</h5>
+            </div>
+            <div className="col-12 col-sm-11 offset-sm-1">
+              <div className="btn-group" role="group">
+                <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+              </div>
+            </div>
+          </div>
+        </>
+      )
+    }
+
+    const FeedBackForm = () => {
+      return (
         <div className="row row-content">
           <div className="col-12">
             <h3>Send us your feedback</h3>
           </div>
           <div className="col-12 col-md-9">
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
 
               <FormGroup row>
                 <Label htmlFor="firstname" md={2}>First name</Label>
@@ -126,7 +150,15 @@ class Contact extends Component {
           </div>
         </div>
 
-      </div>
+      )
+    }
+
+    return (
+      <div className="container" >
+        <BreadCrumb />
+        <ContactUsInfos />
+        <FeedBackForm />
+      </div >
     );
   }
 }
