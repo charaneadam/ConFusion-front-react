7e180c8db6660b3a4bab131b4c81cd2f42180953
Adam Charane <adam.charane@nimbleways.com>
2020-05-15 21:53:14 +0000

    Reset Contact form action

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { actions } from 'react-redux-form';
 
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
@@ -24,14 +25,11 @@ const mapStoreToprops = state => {
 
 const mapDispatchToPtops = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback')) }
 });
 
 class Main extends Component {
-
-  constructor(props) {
-    super(props);
-  }
 
   componentDidMount(){
     this.props.fetchDishes();
@@ -68,7 +66,7 @@ class Main extends Component {
           <Route path="/home" component={HomePage} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
-          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
           <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
           <Redirect to="/home" />
         </Switch>
